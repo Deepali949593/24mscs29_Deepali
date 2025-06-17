c6634df908668a59e0efb172f704f21e913f95c1
Deepali.Sexitgit config --global user.name Deepali.S <6767deepa@gmail.com>
2025-06-17 16:45:50 +0530

    Added Library Management

---
+++ src/main/java/Day2_24mscs29/FictionBook.java
@@ -0,0 +1,16 @@
+package Day2_24mscs29;
+
+public class FictionBook extends Book {
+    private String genre;
+
+    public FictionBook(String title, String author, String genre) {
+        super(title, author);
+        this.genre = genre;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Genre: " + genre);
+    }
+}
